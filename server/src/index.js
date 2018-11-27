3e9e8456bdf458f0d8fd96058b6fc347f1bc6cc8
Justin Kaseman <justinkaseman@live.com>
2018-11-27 14:22:29 -0800

    Link CRUD

--- server/src/index.js
+++ server/src/index.js
@@ -9,36 +9,43 @@ let links = [
   },
 ];
 
-// Schema
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
-
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-}
-`;
-
+let idCount = links.length;
 // Resolvers
 const resolvers = {
   Query: {
     info: () => `This is the API of a HackerNews clone`,
     feed: () => links,
+    link: (root, args) => root,
   },
-  Link: {
-    id: root => root.id,
-    description: root => root.description,
-    url: root => root.url,
+  Mutation: {
+    postLink: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (root, args) => {
+      const index = links.indexOf(links.reduce(link => link.id === args.id));
+      let linkToUpdate = links[index];
+      if (args.id) linkToUpdate.id = args.id;
+      if (args.description) linkToUpdate.description = args.description;
+      return linkToUpdate;
+    },
+    deleteLink: (root, args) => {
+      const index = links.indexOf(links.reduce(link => link.id === args.id));
+      const output = links[index];
+      links.splice(index, 1);
+      return output;
+    },
   },
 };
 
 // Server
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: `server/src/schema.graphql`,
   resolvers,
 });
 
